8be96c822134c92d1a0aad7001ffa12bc049ad0a
Sudharsan <specsudharsan@gmail.com>
2022-04-08 18:22:06 +0530

    day 1 challenge

--- JavaScript/day_1/introduction.js
+++ JavaScript/day_1/introduction.js
@@ -1,1 +1,5 @@
-console.log("Welcome to 30days of javascript");
+// console.log("Welcome to 30days of javascript");
+let firstName
+console.log("firstname",firstName);
+let emptyvalue=null;
+console.log(emptyvalue);
