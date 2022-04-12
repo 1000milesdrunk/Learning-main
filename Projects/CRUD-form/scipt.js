ec93baf6ed834011483a90f0cc1a823cfc57b482
Sudharsan <specsudharsan@gmail.com>
2022-04-12 14:54:55 +0530

    stored in local storage

--- Projects/CRUD-form/scipt.js
+++ Projects/CRUD-form/scipt.js
@@ -1,17 +1,24 @@
-function formdata()
-{
+  function formdata(){
     let name=document.getElementById("name").value;
-    console.log(name);
+    // console.log(name)
     let empno=document.getElementById("empno").value;
-    console.log(empno);
+    // console.log(empno)
     let country=document.getElementById("country").value;
-    console.log(country);
+    // console.log(country)
     let age=document.getElementById("age").value;
-    console.log(age);
+    // console.log(age);
     let phone=document.getElementById("phone").value;
-    console.log(phone);
+    // console.log(phone);
     let email=document.getElementById("email").value;
-    console.log(email);
+    // console.log(email);
     let aadhar=document.getElementById("aadhar").value;
-    console.log(aadhar);
-}
+    // console.log(aadhar);
+    // console.log("form data")
+    localStorage.setItem("name",name);
+    localStorage.setItem("empno",empno);
+    localStorage.setItem("country",country);
+    localStorage.setItem("age",age);
+    localStorage.setItem("phone",phone);
+    localStorage.setItem("email",email);
+    localStorage.setItem("aadhar",aadhar);
+} 
