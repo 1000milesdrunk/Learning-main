0a1317248f9b5950517360e6ce3d26d3e642e4bc
Sudharsan <specsudharsan@gmail.com>
2022-04-12 16:20:37 +0530

    commit

--- Projects/CRUD-form/scipt.js
+++ Projects/CRUD-form/scipt.js
@@ -1,19 +1,19 @@
   function formdata(){
     let name=document.getElementById("name").value;
-    // console.log(name)
+    console.log(name)
     let empno=document.getElementById("empno").value;
-    // console.log(empno)
+    console.log(empno)
     let country=document.getElementById("country").value;
-    // console.log(country)
+    console.log(country)
     let age=document.getElementById("age").value;
-    // console.log(age);
+    console.log(age);
     let phone=document.getElementById("phone").value;
-    // console.log(phone);
+    console.log(phone);
     let email=document.getElementById("email").value;
-    // console.log(email);
+    console.log(email);
     let aadhar=document.getElementById("aadhar").value;
-    // console.log(aadhar);
-    // console.log("form data")
+    console.log(aadhar);
+    console.log("form data")
     localStorage.setItem("name",name);
     localStorage.setItem("empno",empno);
     localStorage.setItem("country",country);
@@ -21,13 +21,5 @@
     localStorage.setItem("phone",phone);
     localStorage.setItem("email",email);
     localStorage.setItem("aadhar",aadhar);
-
-
-    // for(let i=0;i<localStorage.length;i++)
-    // {
-    //     const key=localStorage.key(i);
-    //     const value=localStorage.getItem(key);
-
-    //     write.innerHTML('${key}:${value}</br>');
-    // }
+    
 } 
