27618607cec18c36aa8a571aa0f83bedfc76353e
Sudharsan <specsudharsan@gmail.com>
2022-04-12 14:28:42 +0530

    js typing

---
+++ Projects/CRUD-form/scipt.js
@@ -0,0 +1,17 @@
+function formdata()
+{
+    let name=document.getElementById("name").value;
+    console.log(name);
+    let empno=document.getElementById("empno").value;
+    console.log(empno);
+    let country=document.getElementById("country").value;
+    console.log(country);
+    let age=document.getElementById("age").value;
+    console.log(age);
+    let phone=document.getElementById("phone").value;
+    console.log(phone);
+    let email=document.getElementById("email").value;
+    console.log(email);
+    let aadhar=document.getElementById("aadhar").value;
+    console.log(aadhar);
+}
