9474e08f48e073a3088080a95a44bf584c7f80e4
Sudharsan <specsudharsan@gmail.com>
2022-04-14 17:33:28 +0530

    js for the display and storing in local is done

--- Projects/Form-table/script.js
+++ Projects/Form-table/script.js
@@ -1,11 +1,47 @@
+function init(){
+    if(localStorage.firstname){
+        document.getElementById("firstName").value=localStorage.firstname;
+    }
+    else{
+        alert("Input not entered please check the input")
+    }
+}
 function onFormSubmit(){
+    tableLoading();
+    localStorage.firstname=document.getElementById("firstname").value;
+    localStorage.lastname=document.getElementById("lastname").value;
+    localStorage.phoneNo=document.getElementById("phone").value;
+    localStorage.emailId=document.getElementById("email").value;
+    localStorage.dOB=document.getElementById("dob").value;
+    // alert("Saved Successfully");
+}
+
+function tableLoading(){
     
-    let firstName=document.getElementById("firstName");
-    let lastName=document.getElementById("lastName");
-    let phone=document.getElementById("phone");
-    let email=document.getElementById("email");
-    let dob=document.getElementById("dob");
-    
+    // if(document.getElementById("firstname").value===""){
+    //     alert("enter the input");
+    // }
+    // else if(document.getElementById("lastname").value===""){
+    //     alert("enter the input");
+    // }
+    // else if(document.getElementById("phone").value===""){
+    //     alert("enter the input");
+    // }
+    // else if(document.getElementById("email").value===""){
+    //     alert("enter the input");
+    // }
+    // else if(document.getElementById("dob").value===""){
+    //     alert("enter the input");
+    // }
+
+    let firstName=document.getElementById("firstname").value;
+    let lastName=document.getElementById("lastname").value;
+    let phone=document.getElementById("phone").value;
+    let email=document.getElementById("email").value;
+    let dob=document.getElementById("dob").value;
+    // console.log(firstName);
+    console.log(dob);
+
     var table=document.getElementById("tabledata");
     var row=table.insertRow();
 
@@ -20,5 +56,11 @@ function onFormSubmit(){
     phoneCell.innerHTML=phone;
     emailCell.innerHTML=email;
     dobcell.innerHTML=dob;
+
+    document.getElementById("firstname").value="";
+    document.getElementById("lastname").value="";
+    document.getElementById("phone").value="";
+    document.getElementById("email").value="";
+    document.getElementById("dob").value="";
 
 }
