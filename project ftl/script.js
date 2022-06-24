2f76ef9e4c4515bca095898def98172b5a0767f9
Sudharsan <sudharsan.p@enhancesys.com>
2022-06-24 15:26:31 +0530

    commit

--- project ftl/script.js
+++ project ftl/script.js
@@ -1,15 +1,19 @@
-function addUser(){
+function addUser() {
   let formData = document.getElementById("inputForm");
-  console.log(formData)
-  let userDetails=formData.querySelectorAll("input");
+  console.log(formData);
+  let userDetails = formData.querySelectorAll("input");
   console.log(userDetails);
-  for(let i=0;i<userDetails.length;i++){
-    if(userDetails[0].value=="")
-  }
-  let name=userDetails[0].value;
-  let age=userDetails[1].value;
-  let email=userDetails[2].value;
-  let phone=userDetails[3].value;
-  
- $.post("addUser",{name:name,age:age,email:email,phone:phone},function(response){$("body").html(response)}); 
+
+  let name = userDetails[0].value;
+  let age = userDetails[1].value;
+  let email = userDetails[2].value;
+  let phone = userDetails[3].value;
+
+  $.post(
+    "addUser",
+    { name: name, age: age, email: email, phone: phone },
+    function (response) {
+      $("body").html(response);
+    }
+  );
 }
